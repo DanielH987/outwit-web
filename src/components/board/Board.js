af7d18091a0dceb3b50c545cfd9968ab7abd3553
DanielH987 <hootinid@go.byuh.edu>
2024-06-15 23:04:24 -1000

    feat: Add game board component

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,1 +1,121 @@
-// TODO: Create team component
+import React from 'react';
+import Cell from '../cell/Cell';
+
+const Board = () => {
+    return (
+        <>
+        <div className="board">
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell color={'lightgray'}/>
+            <Cell color={'lightgray'}/>
+            <Cell color={'lightgray'}/>
+        </div>
+        <div className="board">
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell color={'lightgray'}/>
+            <Cell color={'lightgray'}/>
+            <Cell color={'lightgray'}/>
+        </div>
+        <div className="board">
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell color={'lightgray'}/>
+            <Cell color={'lightgray'}/>
+            <Cell color={'lightgray'}/>
+        </div>
+        <div className="board">
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+        </div>
+        <div className="board">
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+        </div>
+        <div className="board">
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+        </div>
+        <div className="board">
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+        </div>
+        <div className="board">
+            <Cell color={'gray'}/>
+            <Cell color={'gray'}/>
+            <Cell color={'gray'}/>
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+        </div>
+        <div className="board">
+            <Cell color={'gray'}/>
+            <Cell color={'gray'}/>
+            <Cell color={'gray'}/>
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+        </div>
+        <div className="board">
+            <Cell color={'gray'}/>
+            <Cell color={'gray'}/>
+            <Cell color={'gray'}/>
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+        </div>
+        </>
+    );
+};
+
+export default Board;
