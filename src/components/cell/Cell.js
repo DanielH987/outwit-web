fcd15e78f5024adf43f960f6d44f0c1515fdc235
DanielH987 <hootinid@go.byuh.edu>
2024-06-15 23:04:20 -1000

    feat: Add color prop to Cell component

--- src/components/cell/Cell.js
+++ src/components/cell/Cell.js
@@ -1,1 +1,10 @@
-// TODO: Create cell component
+import React from 'react';
+
+const Cell = ({ color }) => {
+    return (
+      <div className="cell" style={{ backgroundColor: color }}>
+      </div>
+    );
+  };
+  
+  export default Cell;
