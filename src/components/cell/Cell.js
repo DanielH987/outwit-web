56518dd8d75f80b9fb1f971a2c95bf5a111cebe6
DanielH987 <hootinid@go.byuh.edu>
2024-06-23 20:29:21 -1000

    refactor: Update Cell component to include chip with onClick functionality

--- src/components/cell/Cell.js
+++ src/components/cell/Cell.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Chip from '../chip/Chip';
 
-const Cell = ({ className }) => {
+const Cell = ({ className, chip, onCellClick }) => {
     return (
-        <div className={`cell ${className}`}>
+        <div className={`cell ${className}`} onClick={onCellClick}>
+            {chip && <Chip color={chip.color} onClick={chip.onClick} />}
         </div>
     );
 };
